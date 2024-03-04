1ee667ffc7264548882962bf88f026505d1075e4
SWG-Anthony <Anthony.DeMarco@s.capregboces.org>
2024-03-04 10:12:36 -0500

    exercise 8

---
+++ Exercise 8/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+{
+    Console.WriteLine("Enter maximum number: ");
+    int number = Int32.Parse(Console.ReadLine());
+    for (int i = 0; i < number; i++)
+    {
+        //for loop body
+        if(i % 2 == 0)
+        {
+            Console.WriteLine(i);
+        }
+    }
+    //print asterisk row
+    Console.WriteLine("Asterisks:");
+    Console.ReadLine("How many rows of asterisks");
+
+
+
+  Console.WriteLine(number);
+}
